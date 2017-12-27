cf93f706996086a88ced0e882ad1abb31d770e71
KingDollar <mohamedelgohary1048@gmail.com>
2017-12-28 01:50:22 +0200

    Add files via upload

---
+++ Global.cs
@@ -0,0 +1,19 @@
+﻿namespace SilkroadLauncher
+{
+    using System.Collections.Generic;
+    using System.IO;
+    static class Global
+    {
+        public static Launcher MainForm;
+        public static string path = Directory.GetCurrentDirectory();
+        public static string MediaPassword = "169841";
+
+        public static List<KeyValuePair<string,string>> NoticeDicitonary = new List<KeyValuePair<string, string>>();
+
+        //client relation
+        public static string _ClientIP;
+        public static int _ClientPort;
+        public static int _ClientVer;
+
+    }
+}
